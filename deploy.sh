028755e6a574a3ec8f48c63d06c6339502a84ae1
Zev Goldstein <Zev.Goldstein@gmail.com>
2019-02-04 16:03:29 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go mod vendor
+gcloud functions deploy FcfTest --runtime go111 --trigger-event providers/cloud.firestore/eventTypes/document.write --trigger-resource "projects/fb-js-sdk-test/databases/(default)/documents/go-func-test/{docId}"
