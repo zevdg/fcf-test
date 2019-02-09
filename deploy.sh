9c1c724d7a0f8fa7d574cc1dc4a3dfc526dbc6d5
Zev Goldstein <Zev.Goldstein@gmail.com>
2019-02-09 15:21:18 -0500

    test more stuff

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+cd "$(dirname "$0")"
 go mod vendor
 gcloud functions deploy FcfTest --runtime go111 --trigger-event providers/cloud.firestore/eventTypes/document.write --trigger-resource "projects/fb-js-sdk-test/databases/(default)/documents/go-func-test/{docId}"
--- testfunc.go
+++ testfunc.go
@@ -24,20 +24,40 @@ func FcfTest(ctx context.Context, e fcf.Event) error {
 	dump("raw", e.Value)
 	s := "bar"
 	d := &Data{ZeroMe: "foo", NilMe: &s}
+	// d := &Data{}
 	e.Value.Decode(d)
 	dump("clean", d)
 	return nil
 }
 
 type Data struct {
-	Name    string
-	Age     int
-	Office  string
-	Temp    float32
-	IsAdmin bool
-	Time    time.Time
-	NilMe   *string
-	ZeroMe  string
-	ReMap   int `fcf:"other"`
-	Dynamic interface{}
+	Name      string
+	Age       int
+	Office    string
+	Temp      float32
+	IsAdmin   bool
+	Time      time.Time
+	NilMe     *string
+	ZeroMe    string
+	ReMap     int `fcf:"other"`
+	Dynamic   interface{}
+	MapStruct struct {
+		Baz    int
+		Foo    string
+		SubMap struct {
+			X0 string `fcf:"x0"`
+			X1 string `fcf:"x1"`
+		}
+		Inner1 map[string]string      `fcf:"SubMap"`
+		Inner2 map[string]interface{} `fcf:"SubMap"`
+		Inner3 interface{}            `fcf:"SubMap"`
+	} `fcf:"Map"`
+	MapDynamic   map[string]interface{} `fcf:"Map"`
+	MapInterface interface{}            `fcf:"Map"`
+	TypedMap     map[string]string
+	Location     struct {
+		Lat  float32 `fcf:"latitude"`
+		Long float32 `fcf:"longitude"`
+	}
+	Loc2 fcf.GeoPoint `fcf:"Location"`
 }
